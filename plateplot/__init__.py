dd087683e3efff44aacae7ecc67b5632ccd7ea1f
Scott Warchal <scott.warchal@gmail.com>
2021-05-24 22:57:28 +0100

    add transform module

--- plateplot/__init__.py
+++ plateplot/__init__.py
@@ -1,11 +1,11 @@
 """
+![plate-img](https://user-images.githubusercontent.com/10051679/118867629-31ee9500-b8db-11eb-81e0-10ed5328a692.png)
+
 Python functions to plot interative platemaps using [altair](https://altair-viz.github.io/) from pandas DataFrames.
 
 Platemaps should be able to handle anything from 6-384 well plates (1536 potentially incoming).
-
-![test-img](https://user-images.githubusercontent.com/10051679/118867629-31ee9500-b8db-11eb-81e0-10ed5328a692.png)
-
 """
 
 from plateplot.plot import platemap
+from plateplot import transforms
 from plateplot import utils
---
+++ plateplot/transforms.py
@@ -0,0 +1,125 @@
+"""
+Transform and aggregate data before plotting.
+"""
+
+import numpy as np
+
+
+def group(df, well, val, plate):
+    """
+    Parameters
+    -----------
+    df : pandas.DataFrame
+    well : str
+    val : str
+    plate : str
+
+    Returns
+    --------
+    pandas.DataFrameGroupBy
+    """
+    df_sub = df[[well, val, plate]]
+    df_grouped = df_sub.groupby([plate, well], as_index=False)
+    return df_grouped
+
+
+def mean(df, well, val, plate):
+    """
+    Parameters
+    -----------
+    df : pandas.DataFrame
+    well : str
+    val : str
+    plate : str
+
+    Returns:
+    --------
+    pandas.DataFrame
+    """
+    return group(df, well, val, plate).agg(func="mean")
+
+
+def median(df, well, val, plate):
+    """
+    Parameters
+    -----------
+    df : pandas.DataFrame
+    well : str
+    val : str
+    plate : str
+
+    Returns
+    --------
+    pandas.DataFrame
+    """
+    return group(df, well, val, plate).agg(func="median")
+
+
+def std(df, well, val, plate):
+    """
+    Parameters
+    -----------
+    df : pandas.DataFrame
+    well : str
+    val : str
+    plate : str
+
+    Returns
+    --------
+    pandas.DataFrame
+    """
+    return group(df, well, val, plate).agg(func="std")
+
+
+def var(df, well, val, plate):
+    """
+    Parameters
+    -----------
+    df : pandas.DataFrame
+    well : str
+    val : str
+    plate : str
+
+    Returns
+    --------
+    pandas.DataFrame
+    """
+    return group(df, well, val, plate).agg(func="var")
+
+
+def mad(df, well, val, plate):
+    """
+    Parameters
+    -----------
+    df : pandas.DataFrame
+    well : str
+    val : str
+    plate : str
+
+    Returns
+    --------
+    pandas.DataFrame
+    """
+    return group(df, well, val, plate).agg(func="mad")
+
+
+def cv(df, well, val, plate):
+    """
+    Parameters
+    -----------
+    df : pandas.DataFrame
+    well : str
+    val : str
+    plate : str
+
+    Notes
+    ------
+    Coefficient of variation should not be used on z-scored
+    values, or any other values which could be negative.
+
+    Returns
+    --------
+    pandas.DataFrame
+    """
+    cv = lambda x: np.std(x, ddof=1) / np.mean(x) * 100
+    return group(df, well, val, plate).agg(func=cv)
